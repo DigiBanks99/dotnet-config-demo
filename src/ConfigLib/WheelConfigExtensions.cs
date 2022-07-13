4f6d9b6c1ba44752ae297bdc4a0a48f689d6d514
Wilco Boshoff <wilco.boshoff@entelect.co.za>
2022-07-13 14:58:58 +0200

    Step 6: Consolidate IConfiguration re-use
    Make a re-usable method that reads the config values and make sure
    WebApi and ConsoleApp both reference it.

---
+++ src/ConfigLib/WheelConfigExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConfigLib;
+
+public static class WheelConfigExtensions
+{
+    public static string GetWheelConfigValues(this IConfiguration config)
+    {
+        IEnumerable<IConfigurationSection>? pairs = config.GetSection("Wheel").GetChildren();
+        string[] props = pairs.Select(pair => $"\"{pair.Key}\": \"{pair.Value}\"").ToArray();
+        return $"{{\n{string.Join(",\n", props)}\n}}";
+    }
+}
