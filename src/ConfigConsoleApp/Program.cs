4a88c0c2af4609dd7c945b56bfb238f25caf8c23
Wilco Boshoff <wilco.boshoff@entelect.co.za>
2022-07-13 15:15:15 +0200

    Step 8: Adding appsettings.json to ConsoleApp
    For all the familiar goodness, we add appsettings*.json support to our
    ConsoleApp so we can work with familiar paradigms.

--- src/ConfigConsoleApp/Program.cs
+++ src/ConfigConsoleApp/Program.cs
@@ -2,10 +2,19 @@
 
 using ConfigLib;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+
+HostingEnvironment environment = new()
+{
+    EnvironmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")
+};
 
 ConfigurationBuilder configBuilder = new();
 configBuilder.AddEnvironmentVariables()
-    .AddEnvironmentVariables("DOTNET_");
+    .AddEnvironmentVariables("DOTNET_")
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true, true);
 
 IConfiguration config = configBuilder.Build();
 
