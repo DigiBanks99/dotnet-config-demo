0d956f9c0da54d052ea33dd4f04b0c2792e327f1
Wilco Boshoff <wilco.boshoff@entelect.co.za>
2022-07-13 15:45:19 +0200

    Step 10: IOptions<Wheel>
    Now it is getting fun! We use IOptions<Wheel> and bind to the
    configuration so we can get our Wheel configuration injected into our
    app. For ConsoleApp, we introduce DI so we can use it the same way as
    for WebApi.

--- src/ConfigLib/Wheel.cs
+++ src/ConfigLib/Wheel.cs
@@ -1,3 +1,18 @@
 ﻿namespace ConfigLib;
 
-public record Wheel(string Model = "Unset", decimal Diameter = decimal.MinValue);
+public record Wheel
+{
+    public Wheel()
+    {
+        
+    }
+
+    public Wheel(string model = "Unset", decimal diameter = decimal.MinValue)
+    {
+        this.Model = model;
+        this.Diameter = diameter;
+    }
+
+    public string Model { get; init; }
+    public decimal Diameter { get; init; }
+}
