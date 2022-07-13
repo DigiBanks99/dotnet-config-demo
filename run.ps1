d96d543a30a2a524f7be482967fd9354ae075872
Wilco Boshoff <wilco.boshoff@entelect.co.za>
2022-07-13 15:15:15 +0200

    Step 8: Adding appsettings.json to ConsoleApp
    For all the familiar goodness, we add appsettings*.json support to our
    ConsoleApp so we can work with familiar paradigms.

--- run.ps1
+++ run.ps1
@@ -1,8 +1,10 @@
 [CmdletBinding()]
 param (
     [ValidateSet("ConsoleApp", "WebApi")]
-    [string] $project = "ConsoleApp"
+    [string] $project = "ConsoleApp",
+    [ValidateSet("ConsoleApp","WebApi","MyEnvironment")]
+    [string] $launchProfile = "Config$project"
 )
 
-Write-Host "dotnet run --project 'src/Config$project/Config$project.csproj' -c Debug --launch-profile 'Config$project' --watch"
-dotnet run --project "src/Config$project/Config$project.csproj" -c Debug --launch-profile "Config$project" --watch
+Write-Host "dotnet run --project 'src/Config$project/Config$project.csproj' -c Debug --launch-profile $launchProfile --watch"
+dotnet run --project "src/Config$project/Config$project.csproj" -c Debug --launch-profile $launchProfile --watch
--- src/ConfigConsoleApp/Program.cs
+++ src/ConfigConsoleApp/Program.cs
@@ -2,10 +2,18 @@
 
 using ConfigLib;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting.Internal;
+
+HostingEnvironment environment = new()
+{
+    EnvironmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Development"
+};
 
 ConfigurationBuilder configBuilder = new();
 configBuilder.AddEnvironmentVariables()
-    .AddEnvironmentVariables("DOTNET_");
+    .AddEnvironmentVariables("DOTNET_")
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true, true);
 
 IConfiguration config = configBuilder.Build();
 
