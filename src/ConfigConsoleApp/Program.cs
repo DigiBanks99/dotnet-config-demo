8b11c18127937621c86c3cb54ad405b6b221ca0c
Wilco Boshoff <wilco.boshoff@entelect.co.za>
2022-07-13 14:50:25 +0200

    Step 5: Console ConfigurationBuilder
    Added ConfigurationBuilder to the ConsoleApp so we can read
    IConfiguration values so we have a similar experience to WebApi.

--- src/ConfigConsoleApp/Program.cs
+++ src/ConfigConsoleApp/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.Extensions.Configuration;
+
+ConfigurationBuilder configBuilder = new();
+configBuilder.AddEnvironmentVariables();
+
+IConfiguration config = configBuilder.Build();
+var pairs = config.GetSection("Wheel")
+    .GetChildren();
+
 Console.WriteLine("Wheels: ");
-Console.WriteLine("Model: " + Environment.GetEnvironmentVariable("Wheel:Model"));
-Console.WriteLine("Diameter: " + Environment.GetEnvironmentVariable("Wheel:Diameter"));
+string[] props = pairs.Select(pair => $"\"{pair.Key}\": \"{pair.Value}\"").ToArray();
+Console.WriteLine($"{{\n{string.Join(",\n", props)}\n}}");
