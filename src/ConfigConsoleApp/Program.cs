4d24c781081824e0dd5b0130472392d1ba8e7447
Wilco Boshoff <wilco.boshoff@entelect.co.za>
2022-07-13 14:47:38 +0200

    Step 4: Console Environment variables
    Read config from environment variables directly.

--- src/ConfigConsoleApp/Program.cs
+++ src/ConfigConsoleApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Wheels: ");
-Console.WriteLine(Environment.GetEnvironmentVariable("Wheel:F1"));
+Console.WriteLine("Model: " + Environment.GetEnvironmentVariable("Wheel:Model"));
+Console.WriteLine("Diameter: " + Environment.GetEnvironmentVariable("Wheel:Diameter"));
